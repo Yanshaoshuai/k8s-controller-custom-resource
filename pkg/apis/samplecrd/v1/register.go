c3fc39162fe384218d0ac43cc287785d9afec440
yanshaoshuai <ahutyss@gmail.com>
2022-04-24 00:16:04 +0800

    generate crd common code

--- pkg/apis/samplecrd/v1/register.go
+++ pkg/apis/samplecrd/v1/register.go
@@ -7,11 +7,21 @@ import (
 	"k8s.io/apimachinery/pkg/runtime/schema"
 )
 
+var (
+	SchemeBuilder = runtime.NewSchemeBuilder(addKnownTypes)
+	AddToScheme   = SchemeBuilder.AddToScheme
+)
+
 // GroupVersion is the identifier for the API which includes
 // the name of the group and the version of the API
 var SchemeGroupVersion = schema.GroupVersion{
 	Group:   samplecrd.GroupName,
 	Version: samplecrd.Version,
+}
+
+// Resource takes an unqualified resource and returns a Group qualified GroupResource
+func Resource(resource string) schema.GroupResource {
+	return SchemeGroupVersion.WithResource(resource).GroupResource()
 }
 
 /// addKnownTypes adds our types to the API scheme by registering
